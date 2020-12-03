2b65f8dd2fe2d29f3d9fa1a7b265749ea5fbd0ed
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2020-12-03 20:49:48 +0100

    Update preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import "../node_modules/@patternfly/react-core/dist/styles/base.css";
+//import "../node_modules/@patternfly/react-core/dist/styles/base.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
