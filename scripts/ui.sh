827d471b9d45a8f6e197e33ba29372f064751b94
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-12-25 19:10:26 +0100

    fix

--- scripts/ui.sh
+++ scripts/ui.sh
@@ -1,10 +1,10 @@
 until $(curl --output /dev/null --silent --head --fail http://localhost:8080); do
-    printf '.'
+    printf 'backend...'
     sleep 3
 done
 
 until $(curl --output /dev/null --silent --head --fail http://localhost:8180); do
-    printf '.'
+    printf 'sso...'
     sleep 3
 done
 
