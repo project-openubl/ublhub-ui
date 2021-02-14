9f3c9376ec18fea85cca242878730389c8fecaf8
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-02-14 15:53:15 +0100

    Update about

--- src/layout/AppAboutModal/AppAboutModal.tsx
+++ src/layout/AppAboutModal/AppAboutModal.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import {
   AboutModal,
+  Flex,
+  FlexItem,
+  List,
+  ListItem,
   TextContent,
-  TextList,
-  TextListItem,
 } from "@patternfly/react-core";
+import {
+  GithubIcon,
+  GlobeIcon,
+  EnvelopeIcon,
+  InfoAltIcon,
+} from "@patternfly/react-icons";
+
 import brandImage from "images/logo-navbar.svg";
 
 export interface AppAboutModalProps {
@@ -25,16 +34,91 @@ export const AppAboutModal: React.FC<AppAboutModalProps> = ({
       brandImageAlt="Logo"
       productName=""
     >
-      <TextContent>
-        <TextList component="dl">
-          <TextListItem component="dt">Source code</TextListItem>
-          <TextListItem component="dd">
-            <a href="https://github.com/project-openubl/xsender-server-ui">
-              Github
-            </a>
-          </TextListItem>
-        </TextList>
+      <TextContent className="pf-u-py-xl">
+        <h4>About</h4>
+        <p>
+          <a
+            href="https://project-openubl.github.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            XSender server
+          </a>{" "}
+          allows you to automatize the process of sending your XML files to the
+          SUNAT.
+        </p>
       </TextContent>
+      <TextContent>
+        <h4>Links</h4>
+        <List>
+          <ListItem>
+            <a
+              href="https://project-openubl.github.io/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Website
+            </a>
+          </ListItem>
+          <ListItem>
+            <a
+              href="https://project-openubl.github.io/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Documentation
+            </a>
+          </ListItem>
+        </List>
+      </TextContent>
+      <div className="pf-c-about-modal-box__strapline">
+        <Flex>
+          <FlexItem>
+            <a
+              href="https://github.com/project-openubl/xsender-server"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <i>
+                <GithubIcon />
+              </i>{" "}
+              Source
+            </a>
+          </FlexItem>
+          <FlexItem>
+            <a
+              href="https://projectopenubl.zulipchat.com/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <i>
+                <GlobeIcon />
+              </i>{" "}
+              Discussion forum
+            </a>
+          </FlexItem>
+          <FlexItem>
+            <a href="mailto:projectopenubl+subscribe@googlegroups.com?subject=Subscribe&body=Hello">
+              <i>
+                <EnvelopeIcon />
+              </i>{" "}
+              Mailing list
+            </a>
+          </FlexItem>
+          <FlexItem>
+            <a
+              href="https://github.com/project-openubl/xsender-server/issues"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <i>
+                <InfoAltIcon />
+              </i>{" "}
+              Issue tracking
+            </a>
+          </FlexItem>
+        </Flex>
+      </div>
     </AboutModal>
   );
 };
